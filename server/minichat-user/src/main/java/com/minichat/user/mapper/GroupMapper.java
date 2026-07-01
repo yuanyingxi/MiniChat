9fb04dfd22e97e7be88fb6927d3a088b5d04de3a
GhostSharksDog <1766505342@qq.com>
2026-07-01 21:06:54 +0800

    feat: gateway module only, CORS allow all origins

--- server/minichat-user/src/main/java/com/minichat/user/mapper/GroupMapper.java
+++
@@ -1,7 +0,0 @@
-package com.minichat.user.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.minichat.user.entity.Group;
-
-public interface GroupMapper extends BaseMapper<Group> {
-}
--- server/minichat-user/src/main/java/com/minichat/user/mapper/GroupMemberMapper.java
+++
@@ -1,7 +0,0 @@
-package com.minichat.user.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.minichat.user.entity.GroupMember;
-
-public interface GroupMemberMapper extends BaseMapper<GroupMember> {
-}
